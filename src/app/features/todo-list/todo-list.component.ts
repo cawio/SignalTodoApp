11ab4602401bd6d7cb47053d7a2cfa2d2ce50359
Simon Berner <simon.berner@h-net.com>
2024-01-20 11:53:35 +0100

    feat: Add dialog for adding todos

--- src/app/features/todo-list/todo-list.component.ts
+++ src/app/features/todo-list/todo-list.component.ts
@@ -7,6 +7,9 @@ import { MatInputModule } from '@angular/material/input';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { TodoService } from '../../services/todo.service';
 import { TodoComponent } from '../todo/todo.component';
+import { MatDialog } from '@angular/material/dialog';
+import { AddTodoDialogComponent } from '../add-todo-dialog/add-todo-dialog.component';
+import { config } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
@@ -27,7 +30,7 @@ export class TodoListComponent {
   todos = this.todoService.filteredTodos;
   todoFilter = this.todoService.todoFilter;
 
-  constructor(private todoService: TodoService) { }
+  constructor(private todoService: TodoService, private dialog: MatDialog) { }
 
   public onTodoDeleteClicked(id: number): void {
     this.todoService.deleteTodoById(id);
@@ -54,6 +57,21 @@ export class TodoListComponent {
   }
 
   public onAddTodoClicked(): void {
-
+    this.dialog
+      .open(AddTodoDialogComponent, {
+        width: '400px',
+      })
+      .afterClosed()
+      .subscribe((result: { title: string, description: string }) => {
+        if (result) {
+          this.todoService.addTodo({
+            id: 0,
+            title: result.title,
+            description: result.description,
+            isCompleted: false,
+            date: new Date(),
+          });
+        }
+      });
   }
 }
